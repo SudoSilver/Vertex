561358109713f1255f03a1ec37aff0256b72e54a
SudoSilver <silver1000101@gmail.com>
2026-05-22 18:37:29 +0300

    Fixed some missing dependencies not all still will not compile

--- src/backend/ast/parser/mod.rs
+++ src/backend/ast/parser/mod.rs
@@ -1,1 +1,11 @@
 pub mod parser_trait;
+pub mod parse_stmt;
+pub mod parse_unary;
+pub mod parse_val;
+pub mod parser_navigation;
+
+pub use self::parser_trait::Parser;
+pub use self::parser_navigation::ParserNavigation;
+pub use self::parse_val::ParserDeclarations;
+pub use self::parse_unary::ParseUnary;
+pub use self::parse_stmt::ParseStatments;
